b3b8fb02cd99a602c64527b20e1b42eb39ac52a5
Jackson Ramos <jackramos021495@gmail.com>
2020-05-14 17:44:12 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,8 +2,18 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
   # code goes here
-  def initialize(name)
+  @@all =[]
+  def initialize(name, owner = @owner, mood = "nervous")
     @name = name
+    @owner = owner
+    @mood = mood
+    @@all << self
+  end
+
+  
+
+  def self.all
+    @@all
   end
 
 
