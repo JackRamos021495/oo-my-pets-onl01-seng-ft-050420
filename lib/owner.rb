fe111e27b4ef48ff10df316de805c6d4d4e13456
Jackson Ramos <jackramos021495@gmail.com>
2020-05-14 17:43:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
   # code goes here
+  attr_reader :name
+  @@all = []
+  @@count = 0
+  def initialize(name)
+    @name = name
+  end
+
+  def species(species="human")
+    species
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def save
+    @@all << self
+    @@count += 1
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def reset_all
+
+  end
+
+
+
 end
